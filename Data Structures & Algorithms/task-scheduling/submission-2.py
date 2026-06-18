3d29e46cc672f625356b8c464d7e8b98527d667a
Saimon Neupane <160098770+SaimonNeupane@users.noreply.github.com>
2026-06-18 08:13:37 +0545

    Add: task-scheduling - submission-2

---
+++ Data Structures & Algorithms/task-scheduling/submission-2.py
@@ -0,0 +1,34 @@
+import heapq
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        letters=[0]*26
+        queue=list()
+        time=0
+        for t in tasks:
+            letters[ord(t)-65]-=1
+        letters=[count for count in letters if count<0]
+        heapq.heapify(letters)
+        while letters or queue:
+            time+=1   
+
+
+            while queue and queue[0][1]==time:
+                    heapq.heappush(letters,queue[0][0])
+                    queue.pop(0)
+            if letters:
+                y=heapq.heappop(letters)
+                if y+1<0:
+                    queue.append((y+1,time+n+1))
+            
+        return time
+
+
+
+
+        
+
+
+
+
+        
+        
